b6be78d409dc8a192967b6f53cf76a9ac9d1000e
Nickpodski <Nickpodski@gmail.com>
2021-01-27 21:35:29 -0500

    fixed urban dictionary js

---
+++ assets/js/scriptUD.js
@@ -0,0 +1,23 @@
+$("#startSearch").click(function(e) {
+  e.preventDefault();
+  var userInput = $("#textarea1").val().trim();
+  var q = `https://mashape-community-urban-dictionary.p.rapidapi.com/define?term=${userInput}`; 
+  console.log(q);
+  const settings = {
+    "async": true,
+    "crossDomain": true,
+    "url": `${q}`,
+    "method": "GET",
+    "headers": {
+      "x-rapidapi-key": "a81dca7536msh6bc3d3699a87596p1ae89bjsn3961fc20945c",
+      "x-rapidapi-host": "mashape-community-urban-dictionary.p.rapidapi.com",
+    },
+    error: function(){
+      alert("Please enter valid search term!");
+      location.reload();
+    },
+  };
+  $.ajax(settings).done(function (response) {
+    console.log(response);
+  });
+});

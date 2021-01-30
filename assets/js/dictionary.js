9ca688357d6b8214ced597a1d929a36a9def33ab
William Cheausky <williamchenausky@yahoo.com>
2021-01-30 09:37:15 -0500

    link js to html and add api link

--- assets/js/dictionary.js
+++ assets/js/dictionary.js
@@ -1,1 +1,16 @@
+$("#startSearch").click(function(e) {
+    e.preventDefault();
+
+    var userInput = $("#textarea1").val().trim();
+
 var apiKEY = "72594bc0-4725-41da-8a59-a971cbb8960b"
+
+    var queryURL = `https://www.dictionaryapi.com/api/v3/references/collegiate/json/${userInput}?key=${apiKEY}`;
+
+$.ajax ({
+    url: queryURL,
+    method: "GET"
+}).then(function(response){
+    console.log(response);
+});
+});

1879c101c8ab35d74da74dc89a5d09f49bac83e7
Nickpodski <Nickpodski@gmail.com>
2021-02-02 11:06:29 -0500

    got rid of unecessary function

--- assets/js/scriptUD.js
+++ assets/js/scriptUD.js
@@ -49,32 +49,7 @@ function searchUD(userInput) {
   });
 }
 
-function kmpSearch(pattern, text) {
-  if (pattern.length == 0)
-    return 0; 
 
-  var lsp = [0]; 
-  for (var i = 1; i < pattern.length; i++) {
-    var j = lsp[i - 1]; 
-    while (j > 0 && pattern.charAt(i) != pattern.charAt(j))
-      j = lsp[j - 1];
-    if (pattern.charAt(i) == pattern.charAt(j))
-      j++;
-    lsp.push(j);
-  }
-
-  var j = 0;
-  for (var i = 0; i < text.length; i++) {
-    while (j > 0 && text.charAt(i) != pattern.charAt(j))
-      j = lsp[j - 1]; 
-    if (text.charAt(i) == pattern.charAt(j)) {
-      j++; 
-      if (j == pattern.length)
-        return i - (j - 1);
-    }
-  }
-  return -1; // Not found
-}
  
 $("#startSearch").click(function(e) {
     e.preventDefault();  

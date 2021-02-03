369b7abb01338a5704e44584373dca8fa37b4fca
shuting <zst9394@outlook.com>
2021-02-03 00:01:34 -0500

    new push change some comments

--- assets/js/scriptUD.js
+++ assets/js/scriptUD.js
@@ -1,5 +1,5 @@
 var results = 2;
-var historyArray =[]; //add this !!!!!i need!!!! 1
+var historyArray =[]; //add this !!!!  NEW 1.
 
 function renderUD(r, word) {
   console.log(r);
@@ -8,10 +8,10 @@ function renderUD(r, word) {
   var w = word.toUpperCase();
   $(newH5).text(w);
   $(newH5).addClass("dictH5");
-  $("#urbanDictionary").append(newH5); //delete next 2 lines PLZ which is wrong( var user; localStorage) 2.
+  $("#urbanDictionary").append(newH5); // I deleted 2 lines  this !!!!NEW 2.
   for (var i = 0; i < results + 1; i++) {
-    var udDefinition = r.list[i].definition;    // new?
-    var udDefinitionSTR = JSON.stringify(udDefinition);  // sb add these 2 lines???
+    var udDefinition = r.list[i].definition;   
+    var udDefinitionSTR = JSON.stringify(udDefinition);  
     var newDef = udDefinitionSTR.replace(/\\r\\n/g, '<br>');
     console.log(newDef);
     var newP = $("<p>");
@@ -21,12 +21,12 @@ function renderUD(r, word) {
   }
 }
 
-function searchUD(userInput) {  // add some NEW !!!! u should copy my this whole function!  3.
-  var savedHistory = JSON.parse(localStorage.getItem("history")); // NEW !!!
-  console.log(userInput);   //NEW!!!
-  historyArray=savedHistory;     //NEW!!!
-  $("#history").empty();   // NEW
-  if (savedHistory !== null){   //if local storage is not empty
+function searchUD(userInput) {  // add NEW !!!! u should copy paste this whole function !  3.
+  var savedHistory = JSON.parse(localStorage.getItem("history")); // NEW !!!.....
+  console.log(userInput); 
+  historyArray=savedHistory;     
+  $("#history").empty();  
+  if (savedHistory !== null){   //if local storage is not empty 
     historyArray.push(userInput);
     localStorage.setItem("history",JSON.stringify(historyArray));
     console.log(historyArray);
@@ -36,7 +36,7 @@ function searchUD(userInput) {  // add some NEW !!!! u should copy my this whole
       a.text(historyArray[j])
       console.log(j,historyArray[j])
       a.addClass("info")
-      $("#history").append(a)  // NEW
+      $("#history").append(a)  
      }
   }
   else {     //if its  empty
@@ -52,7 +52,7 @@ function searchUD(userInput) {  // add some NEW !!!! u should copy my this whole
       a.addClass("info")
       $("#history").append(a)
      }
-  }     // NEW !!! 3.
+  }     // ........NEW !!! 3.
 
   var q = `https://mashape-community-urban-dictionary.p.rapidapi.com/define?term=${userInput}`; 
   console.log(q);
@@ -82,10 +82,10 @@ $("#startSearch").click(function(e) {
       $("#pageOne").hide();
       $("#pageTwo").show();
       $("#pageTwo").removeClass("fade-out");
-    }, 1000);  //AFTER 1000); All new  !!!DELETE those 9 lines and copy this new! 4.
+    }, 1000);  //AFTER 1000); All // NEW !!!!!
     var userInput = $("#textarea1").val().trim();// NEW
-    searchUD(userInput);
-  }); //NEW yes just copy this 3 lines plz
+    searchUD(userInput); // // NEW
+  }); //NEW 
 
   $("#textarea1").keypress(function(event) {
     if (event.keyCode === 13) {
@@ -97,9 +97,9 @@ $("#startSearch").click(function(e) {
   $("#startSearchTwo").click(function(e) {
     e.preventDefault();
     $("#urbanDictionary").empty();
-    var userInput = $("#textarea2").val().trim(); //delete 1 line:(localStorage) PLZ 5.
-    searchUD(userInput);  //only this
-    $("#labelTwo").removeClass("active"); //only this 2 lines left !  Hey sweety !i changed the img webhome.png resolve that 6.
+    var userInput = $("#textarea2").val().trim(); //i deleted 1 line  NEW 5.
+    searchUD(userInput);  // NEW !!! 
+    $("#labelTwo").removeClass("active"); // NEW !!! 6.
   });
 
   $("#history").on("click", "li", function() {
